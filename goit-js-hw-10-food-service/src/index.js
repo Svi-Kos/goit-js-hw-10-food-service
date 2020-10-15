b1e3e03ff8628e643ed640320c2bd33bbae91989
Svi-Kos <svitlanakos@gmail.com>
2020-10-15 17:57:21 +0300

    додає розмітку і функціонал сторінки
    розмітка за допомогою handlebars шаблону.
    в локал сторедж зберігається поточна тема.

--- goit-js-hw-10-food-service/src/index.js
+++ goit-js-hw-10-food-service/src/index.js
@@ -1,1 +1,36 @@
+import menuCardsTpl from './templates/menu-cards.hbs';
+import menu from './menu.json';
 import './styles.css';
+
+const menuListContainer = document.querySelector('.js-menu');
+const makeMenuMarkup = menuCardsTpl(menu);
+menuListContainer.insertAdjacentHTML('afterbegin', makeMenuMarkup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const themeSwitchToggle = document.querySelector('#theme-switch-toggle');
+const bodyTheme = document.querySelector('body');
+
+themeSwitchToggle.addEventListener('change', changeTheme);
+
+const currentTheme = localStorage.getItem('currentTheme');
+
+bodyTheme.classList.add(currentTheme);
+
+if (currentTheme === Theme.DARK) {
+  themeSwitchToggle.checked = true;
+}
+
+function changeTheme(evt) {
+  // themeSwitchToggle.checked = true;
+  const theme = evt.currentTarget.checked
+    ? (bodyTheme.classList.add(Theme.DARK),
+      bodyTheme.classList.remove(Theme.LIGHT))
+    : (bodyTheme.classList.add(Theme.LIGHT),
+      bodyTheme.classList.remove(Theme.DARK));
+
+  localStorage.setItem('currentTheme', bodyTheme.classList);
+}
